9e300846b15e5e0265d57b9ff36dc3c151925900
v_sorokin <v_sorokin@kontur>
2020-02-24 13:46:17 +0500

    feat(localecontext): add  to export

--- packages/react-ui/components/LocaleProvider/LocaleProvider.tsx
+++ packages/react-ui/components/LocaleProvider/LocaleProvider.tsx
@@ -1,17 +1,15 @@
+import warning from 'warning';
 import React, { ReactNode } from 'react';
 
-import { defaultLangCode } from './constants';
-import { LangCodes, LocaleControls } from './types';
-
-const LocaleContext = React.createContext<LocaleProviderProps>({});
+import { defaultLangCode } from '../../lib/locale/constants';
+import { LangCodes, LocaleControls } from '../../lib/locale/types';
+import { LocaleContext } from '../../lib/locale';
 
 export interface LocaleProviderProps {
   locale?: LocaleControls;
-  langCode?: LangCodes;
+  langCode: LangCodes;
   children?: ReactNode;
 }
-
-export const LocaleConsumer = LocaleContext.Consumer;
 
 export class LocaleProvider extends React.Component<LocaleProviderProps> {
   public static __KONTUR_REACT_UI__ = 'LocaleProvider';
@@ -20,6 +18,10 @@ export class LocaleProvider extends React.Component<LocaleProviderProps> {
     locale: {},
     langCode: defaultLangCode,
   };
+
+  componentDidMount(): void {
+    warning(true, "LocaleProvider was deprecated please use 'LocaleContext' instead");
+  }
 
   public render() {
     const { locale, langCode } = this.props;
